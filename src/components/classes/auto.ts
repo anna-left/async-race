ff4f2a2f9d4ac7f98ea8a63cbfbac358fcdd1afe
anna-left <anna_left@mail.ru>
2022-01-07 19:25:11 +0300

    feat: add buttons update, race, reset

--- src/components/classes/auto.ts
+++ src/components/classes/auto.ts
@@ -7,11 +7,14 @@ class Auto {
 
   carElement: HTMLElement;
 
-  constructor(num: number, name: string, color: string, carElement: HTMLElement) {
+  isSelected: boolean;
+
+  constructor(num: number, name: string, color: string, carElement: HTMLElement, isSelected = false) {
     this.num = num;
     this.name = name;
     this.color = color;
     this.carElement = carElement;
+    this.isSelected = isSelected;
   }
 }
 
