99f07fbc4f343cc1c4cb9c441cabc8eb9604a814
anna-left <anna_left@mail.ru>
2022-01-09 11:55:52 +0300

    feat: remove array element and HTML element

--- src/components/removeAuto.ts
+++ src/components/removeAuto.ts
@@ -1,11 +1,20 @@
 import { garage, garageApi } from '../index';
+import { changeСounter } from './changeСounter';
 
 async function removeAuto(index: number) {
-  // const track = document.querySelector(`.track[data-num="${String(index)}"]`) as HTMLElement;
+  const track = document.querySelector(`.track[data-num="${String(index)}"]`) as HTMLElement;
+  const curAutoElement = garage.find((car) => Number(car.id) === index);
+
   const promise = garageApi.deleteCar(index);
   const newAuto = await promise;
+  if (curAutoElement) {
+    const curAutoElementIndex = garage.indexOf(curAutoElement);
+    garage.splice(curAutoElementIndex, 1);
+  }
+  track.remove();
+  changeСounter();
   console.log(newAuto);
-  console.log(garage);
+  // console.log(garage);
 }
 
 export { removeAuto };
